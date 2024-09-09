3ff8ad72e9a659a828ccbbbf407bd09ad4494a77
nagarkarvihaan <nagarkarvihaan@gmail.com>
2024-09-09 19:25:41 -0400

    Initial Django project commit

---
+++ Atlanta_Restaurant_Finder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AtlantaRestaurantFinderConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Atlanta_Restaurant_Finder"
