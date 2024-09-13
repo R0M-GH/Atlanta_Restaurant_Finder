882436dbda6a6ed7ea8c3a3efa62136bcdb7c015
JoshauDerival <97137499+JoshauDerival@users.noreply.github.com>
2024-09-12 22:42:06 -0400

    Login to home
    I have made the login page go to the home html.

--- Atlanta_Restaurant_Finder/views.py
+++ Atlanta_Restaurant_Finder/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def hi(request):
     return render(request, 'Atlanta_Restaurant_Finder/index.html')
+def homeView(request):
+    return render(request, 'Atlanta_Restaurant_Finder/home.html')
--- Restaurant_Finder/urls.py
+++ Restaurant_Finder/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.template.context_processors import request
 from django.urls import path, include
+
+from Atlanta_Restaurant_Finder.views import homeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('Atlanta_Restaurant_Finder.urls'))
+    path('', include('Atlanta_Restaurant_Finder.urls')),
 ]
