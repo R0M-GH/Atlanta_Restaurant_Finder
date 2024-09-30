411648cfdc838ac794434e67cb5e853b5fd66f98
JeshalP <jeshal.patel.21@gmail.com>
2024-09-30 18:24:47 -0400

    Added necessary Urls

--- Atlanta_Restaurant_Finder/urls.py
+++ Atlanta_Restaurant_Finder/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     #path('accounts/', include("django.contrib.auth.urls")),
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
-
+    path('home/', views.homeView),
+    path('map.html', views.mapView),
 ]
+
