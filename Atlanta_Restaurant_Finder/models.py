a8ef55553acbbf71a9f692b72f6e0e82a372e65a
JeshalP <jeshal.patel.21@gmail.com>
2024-09-30 19:38:21 -0400

    Revert "Merge branch 'main' into Jeshal"
    This reverts commit f1c5e518cdc1a505d26ef5df649b279a0e2cbe1c, reversing
    changes made to 388e7221919860cd278e817eeaf4089e08cc7941.

--- Atlanta_Restaurant_Finder/models.py
+++ Atlanta_Restaurant_Finder/models.py
@@ -1,2 +1,17 @@
+from django.db import models
+from Restaurant_Finder.db_connection import db
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.backends import BaseBackend
+
+# Create your models here.
+users_collection = db['users']
 
 
+
+class User:
+    @staticmethod
+    def authenticate(username, password):
+        user_check = users_collection.find_one({"username": username})
+        if user_check is not None and check_password(password, user_check["password"]):
+            return user_check
+        return None
--- Restaurant_Finder/settings.py
+++ Restaurant_Finder/settings.py
@@ -125,6 +125,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 # Google maps API key
 GOOGLE_MAPS_API_KEY = "AIzaSyDUR66ihx1SRQ1oqLWlAnmUFuE9swuiWUI"
+
+AUTHENTICATION_BACKENDS = [
+    'Restaurant_Finder.backendsCustom.MongoDBBackend',  # Path to your custom backend
+]
+
+LOGIN_URL = '/login/'
