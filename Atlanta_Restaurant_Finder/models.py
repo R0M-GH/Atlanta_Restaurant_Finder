11f7e4f50cb86c0b5a46fc9ec61b146776dc58e2
JoshauDerival <97137499+JoshauDerival@users.noreply.github.com>
2024-09-30 15:50:36 -0400

    Wiped out models.py for conflict

--- Atlanta_Restaurant_Finder/models.py
+++ Atlanta_Restaurant_Finder/models.py
@@ -1,15 +1,2 @@
-from django.db import models
-from django.contrib.auth.models import User
 
 
-class Favorite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    place_id = models.CharField(max_length=255)  # Use place_id to uniquely identify the restaurant
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.name} (Favorite)"
-
-    class Meta:
-        unique_together = ('user', 'place_id')
