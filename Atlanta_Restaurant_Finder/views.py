d6a46449b3b39783cb5e28204d8305fd44bfe01c
JeshalP <jeshal.patel.21@gmail.com>
2024-10-01 00:39:42 -0400

    Added explicit login view function in views.py and also added error handling and messages for incorrect login info and if username already exists when trying to create a new account

--- Atlanta_Restaurant_Finder/views.py
+++ Atlanta_Restaurant_Finder/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .forms import RegistrationForm  # Import the form you just crea
-
+from .forms import RegistrationForm, LoginForm # Import the form you just crea
+from django.contrib.auth import authenticate, login, logout
 
 @login_required
 def hi(request):
@@ -21,6 +21,18 @@ def homeView(request):
 def mapView(request):
     return render(request, 'Atlanta_Restaurant_Finder/map.html')
 
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('home-page')
+        else:
+            form = LoginForm()
+            return render(request, 'registration/login.html', {'form': form, 'error': True})
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
 
 def register(request):
     if request.method == 'POST':
@@ -29,6 +41,10 @@ def register(request):
             # Create user with validated data
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
+
+            if User.objects.filter(username=username).exists():
+                return render(request, 'registration/register.html', {"form": form, 'error': True})
+
             user = User.objects.create_user(username=username, password=password)
             # Optionally, you can add more user attributes here
             return redirect("login")
