c615a3e091646e04028712fe7a98ed57bd447577
JoshauDerival <97137499+JoshauDerival@users.noreply.github.com>
2024-10-01 22:10:57 -0400

    Update views.py

--- Atlanta_Restaurant_Finder/views.py
+++ Atlanta_Restaurant_Finder/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
 from .models import Favorites
 from .models import UserProfile
 from django.contrib.auth.hashers import make_password
